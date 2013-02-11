e1127233f2acd22d04dba574fb7a5804b84cd3f5
E. Sammer <esammer@cloudera.com>
2013-02-10 17:46:21 -0800

    Added javadoc for Dataset and DatasetRepository.

--- src/main/java/com/cloudera/data/Dataset.java
+++ src/main/java/com/cloudera/data/Dataset.java
@@ -2,10 +2,34 @@ package com.cloudera.data;
 
 import org.apache.avro.Schema;
 
+/**
+ * A logical representation of a set of data entities.
+ * 
+ * Logically, all datasets have two generic properties: a name, and a schema
+ * that describes an entity of that dataset. Concrete implementations of
+ * {@code Dataset} may support additional properties, mandatory or otherwise, as
+ * needed. {@code Dataset}s are not normally instantiated directly, but managed
+ * by a repository (also implementation-specific).
+ * 
+ * @see DatasetRepository
+ */
 public interface Dataset {
 
+  /**
+   * Get the name of a {@code Dataset}. No guarantees about the format of this
+   * name are made.
+   */
   String getName();
 
+  /**
+   * Get the {@code Dataset}'s associated {@link Schema}. Depending on the
+   * underlying storage system, this schema may be simple (i.e. records made up
+   * of only scalar types) or complex (i.e. containing other records, lists, and
+   * so on). Validation of the supported schemas is performed by the managing
+   * repository, not the {@code Dataset} itself.
+   * 
+   * @return
+   */
   Schema getSchema();
 
 }
--- src/main/java/com/cloudera/data/DatasetRepository.java
+++ src/main/java/com/cloudera/data/DatasetRepository.java
@@ -4,21 +4,47 @@ import java.io.IOException;
 
 import org.apache.avro.Schema;
 
+/**
+ * <p>
+ * A logical repository (storage system) of {@link Dataset}s.
+ * </p>
+ * <p>
+ * Implementations of {@code DatasetRepository} are storage systems that contain
+ * zero or more {@link Dataset}s. A repository acts as a factory, as well as a
+ * registry, of datasets. Users can {@link #create(String, Schema)} a new
+ * {@link Dataset} with a name and schema, or retrieve a handle to an existing
+ * dataset, by name, by way of the {@link #get(String)} method. While not
+ * expressly forbidden, most repositories are expected to support only a single
+ * concrete {@link Dataset} implementation.
+ * </p>
+ * <p>
+ * No guarantees are made as to the durability, reliability, or availability of
+ * the underlying storage. That is, a {@code DatasetRepository} could be on
+ * disk, in memory, or some combination.
+ * </p>
+ * 
+ * @param <DS>
+ *          The concrete implementation of {@link Dataset} on which a repository
+ *          operates.
+ */
 public interface DatasetRepository<DS extends Dataset> {
 
   /**
-   * Get the latest version of a named dataset.
+   * Get the latest version of a named {@link Dataset}. If no dataset with the
+   * provided {@code name} exists, an {@link IOException} is thrown.
    * 
    * @param name
+   *          The name of the dataset.
    * @throws IOException
    */
   DS get(String name) throws IOException;
 
   /**
-   * Create a dataset with the supplied schema. Depending on the underlying
-   * dataset storage, some schemas types or configurations may not be supported.
-   * If an illegal schema is supplied, an exception will be thrown by the
-   * implementing class.
+   * Create a {@link Dataset} with the supplied {@code schema}. Depending on the
+   * underlying dataset storage, some schemas types or configurations may not be
+   * supported. If an illegal schema is supplied, an exception will be thrown by
+   * the implementing class. It is illegal to create a more than one dataset
+   * with a given name. If a duplicate name is provided, an exception is thrown.
    * 
    * @param name
    *          The fully qualified dataset name
