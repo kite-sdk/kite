883ae2880a12cb13b0fc0fd6404e42ec057003d4
E. Sammer <esammer@cloudera.com>
2013-02-17 00:54:47 -0800

    Added a test of PartitionedDatasetWriter.

---
+++ src/test/java/com/cloudera/data/TestPartitionedDatasetWriter.java
@@ -0,0 +1,65 @@
+package com.cloudera.data;
+
+import java.io.IOException;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData.Record;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cloudera.data.hdfs.HDFSDatasetRepository;
+import com.google.common.io.Closeables;
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
+
+public class TestPartitionedDatasetWriter {
+
+  private Path testDirectory;
+  private FileSystem fileSystem;
+  private Schema testSchema;
+  private HDFSDatasetRepository repo;
+  private PartitionedDatasetWriter<Object> writer;
+
+  @Before
+  public void setUp() throws IOException {
+    testDirectory = new Path(Files.createTempDir().getAbsolutePath());
+    fileSystem = FileSystem.get(new Configuration());
+    testSchema = new Schema.Parser().parse(Resources.getResource(
+        "user-partitioned.avsc").openStream());
+    repo = new HDFSDatasetRepository(fileSystem, testDirectory);
+    writer = new PartitionedDatasetWriter<Object>(repo.create("users",
+        testSchema));
+  }
+
+  @After
+  public void tearDown() throws IOException {
+    fileSystem.delete(testDirectory, true);
+  }
+
+  @Test
+  public void testBasicOpenClose() throws IOException {
+    writer.open();
+    writer.close();
+  }
+
+  @Test
+  public void testWriter() throws IOException {
+    Record record = new GenericRecordBuilder(testSchema).set("username",
+        "test1").build();
+
+    try {
+      writer.open();
+      writer.write(record);
+      writer.flush();
+      writer.close();
+    } finally {
+      Closeables.close(writer, true);
+    }
+  }
+
+}
