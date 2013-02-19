1b8093f44653a8e1430f90ab96f297fa5be67cbf
E. Sammer <esammer@cloudera.com>
2013-02-18 20:51:47 -0800

    Added the basic DatasetReader methods and updated HDFSDatasetReader to implement them.

--- src/main/java/com/cloudera/data/DatasetReader.java
+++ src/main/java/com/cloudera/data/DatasetReader.java
@@ -1,5 +1,15 @@
 package com.cloudera.data;
 
+import java.io.IOException;
+
 public interface DatasetReader<E> {
+
+  void open() throws IOException;
+
+  boolean hasNext();
+
+  E read() throws IOException;
+
+  void close() throws IOException;
 
 }
--- src/main/java/com/cloudera/data/hdfs/HDFSDatasetReader.java
+++ src/main/java/com/cloudera/data/hdfs/HDFSDatasetReader.java
@@ -8,10 +8,11 @@ import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.cloudera.data.DatasetReader;
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 
-public class HDFSDatasetReader implements Closeable {
+public class HDFSDatasetReader<E> implements DatasetReader<E>, Closeable {
 
   private FileSystem fileSystem;
   private Path path;
@@ -25,11 +26,25 @@ public class HDFSDatasetReader implements Closeable {
     state = State.NEW;
   }
 
+  @Override
   public void open() {
     Preconditions.checkState(state.equals(State.NEW),
         "A reader may not be opened more than once - current state:%s", state);
 
     state = State.OPEN;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return false;
+  }
+
+  @Override
+  public E read() {
+    Preconditions.checkState(state.equals(State.OPEN),
+        "Attempt to read from a file in state:%s", state);
+
+    return null;
   }
 
   @Override
@@ -39,6 +54,7 @@ public class HDFSDatasetReader implements Closeable {
     }
 
     logger.debug("Closing reader on path:{}", path);
+    state = State.CLOSED;
   }
 
   @Override
