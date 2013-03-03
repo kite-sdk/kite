bb70f33a1ed305a4c83cac61dc4bb1f2320e35f4
E. Sammer <esammer@cloudera.com>
2013-03-03 00:37:59 -0800

    Disallow instantiation of the ill-fated Paths.

--- src/main/java/com/cloudera/data/hdfs/util/Paths.java
+++ src/main/java/com/cloudera/data/hdfs/util/Paths.java
@@ -8,6 +8,10 @@ import com.google.common.base.Preconditions;
 
 public class Paths {
 
+  /* Disallow instantiation. */
+  private Paths() {
+  }
+
   public static File toFile(Path path) {
     Preconditions.checkArgument(path != null, "Path can not be null");
 
