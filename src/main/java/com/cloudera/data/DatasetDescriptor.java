ecd4ac571365d77d9c87b36b1f4d01f23ed3b2c7
E. Sammer <esammer@cloudera.com>
2013-03-06 14:06:59 -0800

    Added javadoc for DatsetDescriptor.

--- src/main/java/com/cloudera/data/DatasetDescriptor.java
+++ src/main/java/com/cloudera/data/DatasetDescriptor.java
@@ -6,11 +6,26 @@ import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Supplier;
 
+/**
+ * <p>
+ * The structural definition of a {@link Dataset}.
+ * </p>
+ * <p>
+ * Each {@code Dataset} has an associated {@link Schema} and optional
+ * {@link PartitionStrategy} defined at the time of creation. Instances of this
+ * class are used to hold this information. Users are strongly encouraged to use
+ * the inner {@link Builder} to create new instances.
+ * </p>
+ */
 public class DatasetDescriptor {
 
   private Schema schema;
   private PartitionStrategy partitionStrategy;
 
+  /**
+   * Create an instance of this class with the supplied {@link Schema} and
+   * optional {@link PartitionStrategy} (nullable).
+   */
   public DatasetDescriptor(Schema schema, PartitionStrategy partitionStrategy) {
     this.schema = schema;
     this.partitionStrategy = partitionStrategy;
@@ -30,21 +45,39 @@ public class DatasetDescriptor {
         .add("partitionStrategy", partitionStrategy).toString();
   }
 
+  /**
+   * A fluent builder to aid in the construction of {@link DatasetDescriptor}s.
+   */
   public static class Builder implements Supplier<DatasetDescriptor> {
 
     private Schema schema;
     private PartitionStrategy partitionStrategy;
 
+    /**
+     * Configure the dataset's schema. Required.
+     * 
+     * @return An instance of the builder for method chaining.
+     */
     public Builder schema(Schema schema) {
       this.schema = schema;
       return this;
     }
 
+    /**
+     * Configure the dataset's partitioning strategy. Optional.
+     * 
+     * @param partitionStrategy
+     * @return
+     */
     public Builder partitionStrategy(PartitionStrategy partitionStrategy) {
       this.partitionStrategy = partitionStrategy;
       return this;
     }
 
+    /**
+     * Get an instance of the configured dataset descriptor. Subsequent calls
+     * will produce new instances that are similarly configure.
+     */
     @Override
     public DatasetDescriptor get() {
       Preconditions.checkState(schema != null,
