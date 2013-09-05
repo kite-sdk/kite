7af70461c305afa4eb6a77a23541e024729545ed
Wolfgang Hoschek <whoschek@cloudera.com>
2013-09-04 22:59:30 -0700

    add logging and metrics to dropRecord command

--- cdk-morphlines/cdk-morphlines-core/src/main/java/com/cloudera/cdk/morphline/stdlib/DropRecordBuilder.java
+++ cdk-morphlines/cdk-morphlines-core/src/main/java/com/cloudera/cdk/morphline/stdlib/DropRecordBuilder.java
@@ -22,7 +22,9 @@ import com.cloudera.cdk.morphline.api.Command;
 import com.cloudera.cdk.morphline.api.CommandBuilder;
 import com.cloudera.cdk.morphline.api.MorphlineContext;
 import com.cloudera.cdk.morphline.api.Record;
+import com.cloudera.cdk.morphline.base.AbstractCommand;
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
 /**
  * Command that silently consumes records without ever emitting any record - think /dev/null.
@@ -36,18 +38,23 @@ public final class DropRecordBuilder implements CommandBuilder {
 
   @Override
   public Command build(Config config, Command parent, Command child, MorphlineContext context) {
-    return new DropRecord(config, parent);
+    if (config == null) {
+      return new DevNull(parent);
+    } else {
+      return new DropRecord(config, parent, child, context);
+    }
   }
   
   
   ///////////////////////////////////////////////////////////////////////////////
   // Nested classes:
   ///////////////////////////////////////////////////////////////////////////////
-  private static final class DropRecord implements Command {
+  /** Implementation that does not do logging and metrics */
+  private static final class DevNull implements Command {
     
     private Command parent;
     
-    public DropRecord(Config config, Command parent) { 
+    public DevNull(Command parent) { 
       this.parent = parent;
     }
 
@@ -63,6 +70,51 @@ public final class DropRecordBuilder implements CommandBuilder {
     @Override
     public boolean process(Record record) {
       return true;
+    }
+
+  }
+  
+  ///////////////////////////////////////////////////////////////////////////////
+  // Nested classes:
+  ///////////////////////////////////////////////////////////////////////////////
+  /** Implementation that does logging and metrics */
+  private static final class DropRecord extends AbstractCommand {
+    
+    public DropRecord(Config config, Command parent, Command child, MorphlineContext context) {
+      super(ConfigFactory.empty(), parent, new DummyCommand(), context);
+    }
+
+    @Override
+    protected void doNotify(Record notification) {
+    }
+
+    @Override
+    protected boolean doProcess(Record record) {
+      return true;
+    }
+
+  }
+
+
+  ///////////////////////////////////////////////////////////////////////////////
+  // Nested classes:
+  ///////////////////////////////////////////////////////////////////////////////
+  /** Hack because passing child=null to AbstractCommand ctor is illegal */
+  private static final class DummyCommand implements Command {
+    
+    @Override
+    public Command getParent() {
+      throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public void notify(Record notification) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean process(Record record) {
+      throw new UnsupportedOperationException();
     }
 
   }
