79bcc76e410800900601dc4afb58ba680cb32409
Tom White <tom@cloudera.com>
2014-02-06 15:39:15 +0000

    Add an extra test for Crunch.

--- kite-data/kite-data-crunch/src/test/java/org/kitesdk/data/crunch/TestCrunchDatasets.java
+++ kite-data/kite-data-crunch/src/test/java/org/kitesdk/data/crunch/TestCrunchDatasets.java
@@ -94,7 +94,30 @@ public class TestCrunchDatasets {
   }
 
   @Test
-  @SuppressWarnings("deprecation")
+  public void testPartitionedSource() throws IOException {
+    PartitionStrategy partitionStrategy = new PartitionStrategy.Builder().hash(
+        "username", 2).build();
+
+    Dataset<Record> inputDataset = repo.create("in", new DatasetDescriptor.Builder()
+        .schema(USER_SCHEMA).partitionStrategy(partitionStrategy).build());
+    Dataset<Record> outputDataset = repo.create("out", new DatasetDescriptor.Builder()
+        .schema(USER_SCHEMA).format(Formats.PARQUET).build());
+
+    writeTestUsers(inputDataset, 10);
+
+    PartitionKey key = partitionStrategy.partitionKey(0);
+    Dataset<Record> inputPart0 = inputDataset.getPartition(key, false);
+
+    Pipeline pipeline = new MRPipeline(TestCrunchDatasets.class);
+    PCollection<GenericData.Record> data = pipeline.read(
+        CrunchDatasets.asSource(inputPart0, GenericData.Record.class));
+    pipeline.write(data, CrunchDatasets.asTarget(outputDataset), Target.WriteMode.APPEND);
+    pipeline.run();
+
+    Assert.assertEquals(5, datasetSize(outputDataset));
+  }
+
+  @Test
   public void testPartitionedSourceAndTarget() throws IOException {
     PartitionStrategy partitionStrategy = new PartitionStrategy.Builder().hash(
         "username", 2).build();
