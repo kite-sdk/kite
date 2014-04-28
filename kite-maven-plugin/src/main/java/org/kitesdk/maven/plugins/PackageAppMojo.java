7eb3df7c63e52cfc24a440d5460a1368b1dbdacb
Ben Roling <ben.roling@cerner.com>
2014-04-28 08:39:52 -0500

    CDK-406: Use applicationName as workflowName unless other workflowName is specifically provided

--- kite-maven-plugin/src/main/java/org/kitesdk/maven/plugins/PackageAppMojo.java
+++ kite-maven-plugin/src/main/java/org/kitesdk/maven/plugins/PackageAppMojo.java
@@ -98,7 +98,7 @@ public class PackageAppMojo extends AbstractAppMojo {
   /**
    * The name of the workflow.
    */
-  @Parameter(property = "kite.workflowName", defaultValue = "${project.build.finalName}")
+  @Parameter(property = "kite.workflowName")
   private String workflowName;
 
   /**
@@ -146,7 +146,7 @@ public class PackageAppMojo extends AbstractAppMojo {
           libJars.add(libJarPath);
         }
       }
-      Workflow workflow = new Workflow(workflowXml, schemaVersion, workflowName,
+      Workflow workflow = new Workflow(workflowXml, schemaVersion, workflowName != null ? workflowName : applicationName,
           toolClass, args, hadoopConfiguration, hadoopFs, hadoopJobTracker, libJars);
       WorkflowXmlWriter workflowXmlWriter = new WorkflowXmlWriter(encoding);
       workflowXmlWriter.write(workflow);
