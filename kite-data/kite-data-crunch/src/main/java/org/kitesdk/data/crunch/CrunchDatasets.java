ded514bb03816e599bab5c5d976806d8ef833089
Ben Roling <ben.roling@cerner.com>
2014-06-16 17:02:24 -0500

    CDK-474: CrunchDatasets takes Dataset or View URI

--- kite-data/kite-data-crunch/src/main/java/org/kitesdk/data/crunch/CrunchDatasets.java
+++ kite-data/kite-data-crunch/src/main/java/org/kitesdk/data/crunch/CrunchDatasets.java
@@ -15,10 +15,12 @@
  */
 package org.kitesdk.data.crunch;
 
+import java.net.URI;
 import com.google.common.annotations.Beta;
 import org.apache.crunch.Target;
 import org.apache.crunch.io.ReadableSource;
 import org.kitesdk.data.Dataset;
+import org.kitesdk.data.Datasets;
 import org.kitesdk.data.View;
 
 /**
@@ -57,6 +59,34 @@ public class CrunchDatasets {
   public static <E> ReadableSource<E> asSource(View<E> view, Class<E> type) {
     return new DatasetSourceTarget<E>(view, type);
   }
+  
+  
+  /**
+   * Expose the {@link View} or {@link Dataset} represented by the URI
+   * as a Crunch {@link ReadableSource}.
+   *
+   * @param view the URI of the view or dataset to read from
+   * @param type    the Java type of the entities in the dataset
+   * @param <E>     the type of entity produced by the source
+   * @return a {@link ReadableSource} for the view
+   */
+  public static <E> ReadableSource<E> asSource(URI uri, Class<E> type) {
+    View<E> view = Datasets.<E, View<E>>view(uri);
+    return new DatasetSourceTarget<E>(view, type);
+  }
+  
+  /**
+   * Expose the {@link View} or {@link Dataset} represented by the URI
+   * as a Crunch {@link ReadableSource}.
+   *
+   * @param view the URI of the view or dataset to read from
+   * @param type    the Java type of the entities in the dataset
+   * @param <E>     the type of entity produced by the source
+   * @return a {@link ReadableSource} for the view
+   */
+  public static <E> ReadableSource<E> asSource(String uri, Class<E> type) {
+    return asSource(URI.create(uri), type);
+  }
 
   /**
    * Expose the given {@link Dataset} as a Crunch {@link Target}.
@@ -82,5 +112,28 @@ public class CrunchDatasets {
    */
   public static <E> Target asTarget(View<E> view) {
     return new DatasetTarget<E>(view);
+  }
+
+  /**
+   * Expose the {@link Dataset} or {@link View} represented by the given
+   * URI as a Crunch {@link Target}.
+   * 
+   * @param uri the dataset or view URI
+   * @return a {@link Target} for the dataset or view
+   */
+  public static Target asTarget(String uri) {
+    return asTarget(URI.create(uri));
+  }
+  
+  /**
+   * Expose the {@link Dataset} or {@link View} represented by the given
+   * URI as a Crunch {@link Target}.
+   * 
+   * @param uri the dataset or view URI
+   * @return a {@link Target} for the dataset or view
+   */
+  public static Target asTarget(URI uri) {
+    View<Object> view = Datasets.view(uri);
+    return new DatasetTarget<Object>(view);
   }
 }
