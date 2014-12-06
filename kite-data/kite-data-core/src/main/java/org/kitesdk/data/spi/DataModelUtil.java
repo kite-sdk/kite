d732929ca4ad1ce04cdd2cc1cf8322085bc0c354
Ryan Blue <blue@apache.org>
2014-12-05 18:20:46 -0800

    CDK-802: Fix Nullable annotation with AllowNulls.
    This copies the fix for AVRO-1604 into Kite's AllowNulls. The method has
    been renamed to avoid confusion with the static method makeNullable in
    ReflectData.

--- kite-data/kite-data-core/src/main/java/org/kitesdk/data/spi/DataModelUtil.java
+++ kite-data/kite-data-core/src/main/java/org/kitesdk/data/spi/DataModelUtil.java
@@ -18,6 +18,9 @@ package org.kitesdk.data.spi;
 
 import com.google.common.annotations.VisibleForTesting;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
@@ -53,7 +56,28 @@ public class DataModelUtil {
       if (field.getType().isPrimitive()) {
         return schema;
       }
-      return makeNullable(schema);
+      return makeNullableSchema(schema);
+    }
+
+    /** Create and return a union of the null schema and the provided schema. */
+    public static Schema makeNullableSchema(Schema schema) {
+      if (schema.getType() == Schema.Type.UNION) {
+        // check to see if the union already contains NULL
+        for (Schema subType : schema.getTypes()) {
+          if (subType.getType() == Schema.Type.NULL) {
+            return schema;
+          }
+        }
+        // add null as the first type in a new union
+        List<Schema> withNull = new ArrayList<Schema>();
+        withNull.add(Schema.create(Schema.Type.NULL));
+        withNull.addAll(schema.getTypes());
+        return Schema.createUnion(withNull);
+      } else {
+        // create a union with null
+        return Schema.createUnion(Arrays.asList(Schema.create(Schema.Type.NULL),
+            schema));
+      }
     }
   }
 
