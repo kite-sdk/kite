738b397cf1f8b1fbea4fbe9bda017839081d9f37
Wolfgang Hoschek <whoschek@cloudera.com>
2015-04-10 21:33:03 +0300

    add morphline test case for HOCON include statement

--- kite-morphlines/kite-morphlines-core/src/test/java/org/kitesdk/morphline/api/MorphlineTest.java
+++ kite-morphlines/kite-morphlines-core/src/test/java/org/kitesdk/morphline/api/MorphlineTest.java
@@ -155,6 +155,15 @@ public class MorphlineTest extends AbstractMorphlineTest {
   }
   
   @Test
+  public void testParseInclude() throws Exception {
+    morphline = createMorphline("test-morphlines/parseInclude");  
+    Record input = new Record();
+    Record expected = new Record();
+    expected.put("result",  "bar");
+    processAndVerifySuccess(input, expected);
+  }
+  
+  @Test
   public void testParseVariables() throws Exception {
     System.setProperty("ENV_ZK_HOST", "zk.foo.com:2181/solr");
     System.setProperty("ENV_SOLR_URL", "http://foo.com:8983/solr/myCollection");
